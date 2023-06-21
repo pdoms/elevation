40c064c5509e07e715b89fde61322feb41423d23
pdoms <domspaulo@gmail.com>
2023-06-21 20:29:31 +0200

    set up controller

---
+++ src/app/controller.tsx
@@ -0,0 +1,20 @@
+
+//since opentopodata.org provides differnt datasets, the dataset could either
+//be user defined or configurable, if further features should be considered
+const BASEURL = "https://api.opentopodata.org/v1/aster30m?locations="
+
+/*
+ * Function that calls the opentopodata dataset with the location params and returns
+ * the elevation.
+ * */
+export async function makeFetch(): Promise<[]> {
+    
+
+
+    return []
+
+}
+
+function urlBuilder(lat: number, lat: number): string {
+    
+}
